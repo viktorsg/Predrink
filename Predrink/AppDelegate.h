85df0ef1ddc76677080b06dd8a098927de5605e6
Viktor Georgiev <viktor@Viktors-iMac.local>
2017-03-09 14:11:13 +0200

    Initial Commit

---
+++ Predrink/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Predrink
+//
+//  Created by Viktor Georgiev on 3/9/17.
+//  Copyright © 2017 Viktor Georgiev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
