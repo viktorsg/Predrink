2b560c4619a0e064b4ec0e0e4531ef3716e87ada
Viktor Georgiev <viktor@viktors-imac.local>
2017-03-10 11:28:06 +0200

    Test

---
+++ Predrink/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  Predrink
+//
+//  Created by Viktor Georgiev on 3/10/17.
+//  Copyright © 2017 Viktor Georgiev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
